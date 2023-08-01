a732edfd78934e6136a890b7e542c986796a2827
吴禹辉 <46028035+PointWu@users.noreply.github.com>
2023-08-01 19:34:38 +0800

    feat: define the global parameters of the axios request library

--- generated/core/OpenAPI.ts
+++ generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:8121',
+    BASE: 'http://localhost:8121', // 这里改请求地址
     VERSION: '1.0',
     WITH_CREDENTIALS: true,
     CREDENTIALS: 'include',
